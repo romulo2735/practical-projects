81d61f19df6d73bb73b7c9983baf138a143a0665
Romulo Sousa <romulosousa27@hotmail.com>
2021-10-30 11:18:25 -0300

    Update web.php
    Ajustando o namespace das rotas e recursos.

--- educational/routes/web.php
+++ educational/routes/web.php
@@ -29,8 +29,8 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::prefix('admin')->group(function () {
-    require __DIR__ . '/auth.php';
+require __DIR__ . '/auth.php';
 
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('users', UserController::class);
 });
