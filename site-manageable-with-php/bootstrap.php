8fc0075bd18a913b09fc4b9361f33b6468878a64
Romulo Sousa <romulosousa27@hotmail.com>
2022-01-07 11:41:10 -0300

    Estrtutura de importações de arquivo essenciais.

---
+++ site-manageable-with-php/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/config.php';
+require __DIR__ . '/src/handler_error.php';
+require __DIR__ . '/src/route_resolve.php';
+require __DIR__ . '/src/render.php';
+require __DIR__ . '/src/conn.php';
+require __DIR__ . '/src/flash.php';
+require __DIR__ . '/src/auth.php';
