d021a08cee4962a1f9d2d00861a5200adc6421b1
Romulo Sousa <romulosousa27@hotmail.com>
2022-06-15 20:42:39 -0300

    Update connection.js
    atualizando a conexão com o mongo, de acordo com a documentação

--- EmailMarketingNode/src/database/connection.js
+++ EmailMarketingNode/src/database/connection.js
@@ -1,15 +1,9 @@
 let mongoose = require('mongoose');
 
-mongoose.connect(
-    'mongodb://localhost:27017/email_marketing', {
-        useMongoClient: true,
-        function (err) {
-            if(err){
-                console.log('Mogoose Error => ', err);
-            }
-            console.log("Mongoose Connected");
-        }
-    }
-);
+main().catch(err => console.log(`##### Mongoose Error => ${err} ###`));
+
+async function main() {
+    await mongoose.connect('mongodb://mongo:27017/email_marketing');
+}
 
 module.exports = mongoose;
