ea328d5834999d649c885bd18cc8c7f2e0a7f1a2
Romulo Sousa <romulosousa27@hotmail.com>
2022-06-15 21:24:09 -0300

    Update users.js
    Eto create a new user

--- EmailMarketingNode/routes/users.js
+++ EmailMarketingNode/routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let User = require('../src/Models/User');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/register', function (req, res, next) {
+  let dataUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    accounts: [{
+      name: req.body.account_name,
+      role: 'owner',
+      enable: true,
+    }]
+  }
+
+  let callback = (err, User) => {
+    if (err) {
+      return res.status(422).json({
+        err: err
+      });
+    }
+    return res.status(200).json({
+      user: User
+    });
+  }
+
+  User.create(dataUser, callback);
+
 });
 
 module.exports = router;
